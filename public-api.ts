50f49f11559b351fc75d8278db36ce1adb2caf62
Lam Nhan <nhan.lh@raksul.com>
2024-03-10 21:44:03 +0700

    Adopt the universal folder structure

--- public-api.ts
+++ public-api.ts
@@ -1,21 +1,21 @@
-export * from './src/lib/consts/global';
+export * from './lib/consts/global';
 
-export * from './src/lib/decorators/tini-element-components';
-export * from './src/lib/decorators/tini-element-theming';
+export * from './lib/decorators/tini-element-components';
+export * from './lib/decorators/tini-element-theming';
 
-export * from './src/lib/directives/part-attr-map';
+export * from './lib/directives/part-attr-map';
 
-export * from './src/lib/utils/border';
-export * from './src/lib/utils/colors';
-export * from './src/lib/utils/components';
-export * from './src/lib/utils/events';
-export * from './src/lib/utils/factors';
-export * from './src/lib/utils/generic';
-export * from './src/lib/utils/outline';
-export * from './src/lib/utils/scripts';
-export * from './src/lib/utils/styles';
-export * from './src/lib/utils/theme';
-export * from './src/lib/utils/ui';
-export * from './src/lib/utils/varies';
+export * from './lib/utils/border';
+export * from './lib/utils/colors';
+export * from './lib/utils/components';
+export * from './lib/utils/events';
+export * from './lib/utils/factors';
+export * from './lib/utils/generic';
+export * from './lib/utils/outline';
+export * from './lib/utils/scripts';
+export * from './lib/utils/styles';
+export * from './lib/utils/theme';
+export * from './lib/utils/ui';
+export * from './lib/utils/varies';
 
-export * from './src/lib/classes/tini-element';
+export * from './lib/classes/tini-element';
